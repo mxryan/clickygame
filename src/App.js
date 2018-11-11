0d540ca3a0c8b8bef00fbba6ee7cedf8f70f0bfc
M Ryan <mxryan@live.com>
2018-11-11 18:14:13 -0500

    added randomization and score tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./components/Header"
+import ImgCard from "./components/ImgCard"
 
 class App extends Component {
   constructor(props){
@@ -9,43 +10,101 @@ class App extends Component {
       bestScore: 0,
       imgOrder: [{
         imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        clicked: false,
+        id: 0
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/L6qa9Dvb.jpg",
+        clicked: false,
+        id: 1
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/88KWnklb.jpg",
+        clicked: false,
+        id: 2
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/U3bsdylb.jpg",
+        clicked: false,
+        id: 3
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/5MFYgZGb.jpg",
+        clicked: false,
+        id: 4
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/pzA0ljSb.jpg",
+        clicked: false,
+        id: 5
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/G2MKwcGb.jpg",
+        clicked: false,
+        id: 6
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/eagurXGb.jpg",
+        clicked: false,
+        id: 7
       },{
-        imgSrc: "https://i.imgur.com/UMJzRnFb.jpg",
-        clicked: false
+        imgSrc: "https://i.imgur.com/bgifTQlb.jpg",
+        clicked: false,
+        id: 8
       }]
     }
   }
   
+  randomize = () => {
+    const newArrState = [...this.state.imgOrder].sort(function() {
+      return .5 - Math.random();
+    });
+    this.setState({imgOrder: newArrState})
+  }
+
+  handleClick = (e) => {
+    console.log(e.target.id);
+    let picID = e.target.id;
+    const imgOrderCopy = [...this.state.imgOrder]
+    for (let i = 0; i < imgOrderCopy.length; i++) {
+      console.log(imgOrderCopy[i]);
+      if (picID == imgOrderCopy[i].id) {
+        if (imgOrderCopy[i].clicked) {
+          this.gameOver();
+        } else {
+          imgOrderCopy[i].clicked = true;
+          this.setState({
+            imgOrder: imgOrderCopy, 
+            currentScore: this.state.currentScore + 1
+          })
+        }
+      }
+    }
+    console.log(this.state)
+    this.randomize();
+  }
+
+  gameOver = () => {
+    console.log("game over");
+  }
+
   render() {
+    const ImgCards = this.state.imgOrder.map((obj,i) => {
+      return (
+        <ImgCard
+          imgSrc={obj.imgSrc}
+          handleClick={this.handleClick}
+          key={i}
+          id={obj.id}
+        />
+      )
+    })
+    const mainStyle = {
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr 1fr",
+      gridRowGap: "10px"
+    }
     return (
       <div className="App">
         <Header 
           currentScore={this.state.currentScore}
           bestScore={this.state.bestScore}
         />
-        <main className="container"><p>Hey</p></main>
+        <main className="container" style={mainStyle}>
+        {ImgCards}</main>
       </div>
     );
   }
--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,13 +1,20 @@
 import React from "react";
-const ImgCard = () => {
-  return(
-    <div className="card" style="width: 18rem;">
-  <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  </div>
+const ImgCard = (props) => {
+  const w18rem = {
+    width: "18rem"
+  }
+  return (
+    <div className="card" style={w18rem}>
+      <img 
+        className="card-img-top"
+        src={props.imgSrc}
+        alt="A cat." 
+        onClick={props.handleClick}
+        id={props.id}
+      />
+      
+    </div>
 
   )
 }
+export default ImgCard;
