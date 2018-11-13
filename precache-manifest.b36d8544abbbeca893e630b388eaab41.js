ff673d1f810b693178b07e8eaf9df2dc99a275a0
M Ryan <mxryan@live.com>
2018-11-13 14:12:14 -0500

    Updates

---
+++ precache-manifest.b36d8544abbbeca893e630b388eaab41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "a978ff2164be56124d64",
+    "url": "/clickygame/static/js/main.a978ff21.chunk.js"
+  },
+  {
+    "revision": "4ed411020c525a9e3f1b",
+    "url": "/clickygame/static/js/1.4ed41102.chunk.js"
+  },
+  {
+    "revision": "64c116e002c196475be4ef66823468af",
+    "url": "/clickygame/index.html"
+  }
+];
